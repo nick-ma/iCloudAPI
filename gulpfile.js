43d7c95c8efc871ae0132f48d07c6d45c8639e96
Nick Ma <honeyday.mj@gmail.com>
2014-04-07 00:18:45 +0800

    update gulpfile and package.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var pkg = require("./package.json");
-var gulp = requite('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var gutil = require('gulp-util');
+var mocha = require('gulp-mocha');
+
+var watching = false;
+var onError = function (err) {
+    if (watching) {
+        this.emit('end');
+    } else {
+        process.exit(1);
+    }
+};
+gulp.task('lint', function () {
+    return gulp.src(['gulpfile.js', 'lib/*.js', 'test/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function () {
+    return gulp.src('test/*.js')
+        .pipe(mocha({
+            reporter: 'spec'
+        }))
+        .on('error', onError);
+
+});
 
 gulp.task('watch', function () {
-    gulp.watch('lib');
-})
+    watching = true;
+    gulp.watch(['lib/*.js', 'test/*.js'], function () {
+        gulp.run('lint', 'test');
+    });
+});
 
 gulp.task('default', ['watch']);
